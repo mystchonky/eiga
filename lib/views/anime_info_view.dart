772571dedfa9c9195cde6b0e6133d0f425d5ff1e
luiq54 <dev.darshanphaldesai@gmail.com>
2021-01-06 18:52:30 +0530

    landscape overflow fix

--- lib/views/anime_info_view.dart
+++ lib/views/anime_info_view.dart
@@ -33,78 +33,80 @@ class AnimeInfo extends StatelessWidget {
           appBar: AppBar(
             title: Text(animeName),
           ),
-          body: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        flex: 7,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              animeName,
-                              maxLines: 2,
-                              style: Theme.of(context).textTheme.subtitle1,
-                            ),
-                            Text(
-                              anime.title.native,
-                              style: Theme.of(context).textTheme.subtitle2,
-                            )
-                          ],
+          body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 7,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                animeName,
+                                maxLines: 2,
+                                style: Theme.of(context).textTheme.subtitle1,
+                              ),
+                              Text(
+                                anime.title.native,
+                                style: Theme.of(context).textTheme.subtitle2,
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                          flex: 3,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                anime.coverImage.medium,
-                                fit: BoxFit.cover,
-                              )))
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Description",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    constraints: BoxConstraints(maxHeight: 250),
-                    // child: Expanded(
-                    //   flex: 1,
+                        Expanded(
+                            flex: 3,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(
+                                  anime.coverImage.medium,
+                                  fit: BoxFit.cover,
+                                )))
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Description",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      constraints: BoxConstraints(maxHeight: 250),
+                      // child: Expanded(
+                      //   flex: 1,
                       child: SingleChildScrollView(
                         child: Text(
                           anime.description.replaceAll(RegExp('<br>'), ""),
                           style: Theme.of(context).textTheme.bodyText2,
                         ),
                       ),
-  //                  ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RaisedButton.icon(
-                          label: Text("Watch Now"),
-                          icon: Icon(Icons.play_arrow),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                new MaterialPageRoute(
-                                    builder: (context) =>
-                                        FourAnime(search: animeName)));
-                          }),
-                    ],
-                  )
-                ],
-              )),
+                      //                  ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RaisedButton.icon(
+                            label: Text("Watch Now"),
+                            icon: Icon(Icons.play_arrow),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) =>
+                                          FourAnime(search: animeName)));
+                            }),
+                      ],
+                    )
+                  ],
+                )),
+          ),
         );
       },
     );
